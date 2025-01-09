43cd9465fea0e8a5554fdf0278af655178cd8524
ManuelNietoIESbelen <mniejim1703@g.educaand.es>
2025-01-09 12:54:54 +0100

    bootstrap

---
+++ PYTHON/python1.py
@@ -0,0 +1,15 @@
+#Tipos de tipos
+cadena= "Hola soy un string"
+numero= 7
+numero2= 10.5
+booleano= True
+
+tipo=numero+numero2
+print(numero,numero2)
+print(tipo)
+
+
+lista=[12,3,True,"hola",343]
+
+print(lista[3])
+print()
