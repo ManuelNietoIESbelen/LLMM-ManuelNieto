996485a5c3256796243eec332a0ae692c6cd6838
ManuelNietoIESbelen <mniejim1703@g.educaand.es>
2025-02-05 13:42:58 +0100

    js

---
+++ JAVASCRIPT/eventos/assets/codigo.js
@@ -0,0 +1,9 @@
+const p1=document.querySelector('.parrafo');
+const p2=document.querySelector('.parrafo');
+const b=document.querySelector('body');
+
+
+p1.addEventListener("click",()=>alert("parafo 1 clcik "));
+b.addEventListener("load",mensaje);
+
+function mensaje(){alert("ALERTA")}
