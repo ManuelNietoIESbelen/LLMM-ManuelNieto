2460d5b6bcbc4345c93ee2dc4024f69f7a51f877
ManuelNietoIESbelen <mniejim1703@g.educaand.es>
2025-03-03 10:18:22 +0100

    json js

---
+++ JAVASCRIPT/JSON_JS/assets/js.js
@@ -0,0 +1,9 @@
+let nombrePokemon = document.getElementById('name');
+let imagenPokemon = document.getElementById('img');
+let alturaPokemon = document.getElementById('height');
+let tipoPokemon = document.getElementById('type');
+const button = document.getElementById('btn');
+
+button.addEventListener("click",function(){
+    
+})       
