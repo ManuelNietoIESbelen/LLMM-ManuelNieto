095c8684e8d3541341b196b154f3503afc69e84e
“ManuelNietoIESbelen” <mniejim1703@g.educaand.es>
2025-03-05 18:30:21 +0100

    js

--- JAVASCRIPT/JSON_JS2/assets/js.js
+++ JAVASCRIPT/JSON_JS2/assets/js.js
@@ -1,34 +1,49 @@
-let titulo = document.querySelector('.titulo');
-let anyo = document.querySelector('.anyo');
-let duracion = document.querySelector('.duracion');
-const button = document.querySelector('.btn');
-const url = 'https://www.omdbapi.com/?t=titulo&apikey=6bd47da3';
+let titulo = document.querySelector(".titulo");
+let anyo = document.querySelector(".anyo");
+let duracion = document.querySelector(".duracion");
+const button = document.querySelector(".btn");
+const tituloError = document.querySelector(".tituloError");
+const tituloError2 = document.querySelector(".tituloError");
+button.addEventListener("click", getMovie);
 
 
-
-button.addEventListener('click', getMovie);
 
 async function getMovie() {
-    var movieTitle; 
-    element= document.querySelector('#movieTitle').value;
-    
-    if(element != null) {
-        movieTitle = element.value;
-    }else{
-        movieTitle = null;
+    var tituloPeliculaUsuario;
+    element = document.querySelector("#pelicula");
+    if (element != null) {
+        tituloPeliculaUsuario = element.value;
+    } else {
+        tituloPeliculaUsuario = null;
     }
 
+    const url =
+        "https://www.omdbapi.com/?t=" + tituloPeliculaUsuario + "&apikey=6bd47da3";
+
     try {
-        
-    const response = await fetch(url);
-    const data = await response.json();
-    titulo.innerHTML = data.Title;
-    anyo.innerHTML = data.Year;
-    duracion.innerHTML = data.Runtime;
+        const response = await fetch(url);
+        const data = await response.json();
+        if (data.Response === "True") {
+            titulo.innerHTML = data.Title;
+            anyo.innerHTML = data.Year;
+            duracion.innerHTML = data.Runtime;
+            tituloError.innerHTML = "PELICULA ENCONTRADA";
+            tituloError.classList.remove('tituloErrorRojo');
+            tituloError.classList.add('tituloError');
+        } else {
+            titulo.innerHTML = "";
+            anyo.innerHTML = "";
+            duracion.innerHTML = "";
+            tituloError.innerHTML = "ERROR AL ENCONTRAR LA PELICULA";
+            tituloError.classList.remove('tituloError');
+            tituloError.classList.add('tituloErrorRojo');
+
+        }
+
     } catch (error) {
         console.log(error);
-        titulo.innerHTML = 'Movie not found';
-        anyo.innerHTML = '';
-        duracion.innerHTML = '';
+        titulo.innerHTML = "";
+        anyo.innerHTML = "";
+        duracion.innerHTML = "";
     }
 }
